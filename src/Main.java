f4c2e7759c965081cc1bfce942f1fa2bf3e61b11
Ondotteess <osipovichivan99@gmail.com>
2024-10-21 03:09:31 +0700

    pre-tokineze

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import syspro.tm.lexer.*;
-import syspro.tm.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     private static class MyLexer implements Lexer {
@@ -10,21 +8,86 @@ public class Main {
         private int position = 0;
         private int currentIndentLevel = 0;
 
-        private static final Set<String> Symbols = EnumSet.allOf(Symbol.class)
-                .stream()
-                .map(symbol -> symbol.text)
-                .collect(Collectors.toSet()); //
+        private void scan(StringBuilder sb, int start, int end){
+            if (!sb.isEmpty()) {
+                Scanner.scanToken(sb, start, end);      // TODO: убрать копировалки
+                sb.setLength(0);
+            }
+        }
+
+
+        private void trivialWhitespaces(String s, StringBuilder sb, int currentChar) { // TODO: переименовать
+            int start = position;
+            while (position < s.length() && (s.charAt(position) == ' ' || s.charAt(position) == '\t')) {
+                sb.append(s.charAt(position));
+                position += Character.charCount(currentChar);
+            }
+            scan(sb, start, position);  // TODO: тут входная строка может начинаться с пробелов табуляций
+        }
+
+
+        private void newLine(String s, StringBuilder sb, int currentChar){
+            position += Character.charCount(currentChar);
+
+            while (position < s.length()) {
+                currentChar = s.codePointAt(position);
+                if (currentChar == ' ' || currentChar == '\t') {
+                    sb.appendCodePoint(currentChar);
+                    position += Character.charCount(currentChar);
+                } else {
+                    break;      // TODO: тут посмотреть вперед, если впереди НЕ пробел и табуляция - свернуть токен
+                }
+            }
+            scan(sb, 1, 1);
+        }
+
+
+        private void comment(String s, StringBuilder sb, int currentChar) {
+            sb.appendCodePoint(currentChar);
+            position += Character.charCount(currentChar); //TODO: ошибка ли пробелы потом комментарий? ДА
+
+            while (position < s.length()) {
+                currentChar = s.codePointAt(position);
+                if (currentChar == '\n') {
+                    sb.appendCodePoint(currentChar);
+                    scan(sb, 1, 1);
+                    break;
+                } else {
+                    sb.appendCodePoint(currentChar);
+                    position += Character.charCount(currentChar);
+                }
+            }
+            scan(sb, 1, 1);
+        }
 
         @Override
         public List<Token> lex(String s) {
             ArrayList<Token> a = new ArrayList<>();
 
-            while (position < s.length()) {
+            StringBuilder sb = new StringBuilder();
+
+            /*
+             *   Встретили пробел -> добавляем все пробелы и табуляции прямо в токен
+             *   Перенос и дальше пробелы/табуляции-> индентационный токен
+             *   Решётку -> добавляем в токен всё до переноса
+             */
+
+            while (position < s.length()) {                     // TODO: сохранять стартовую и конечную позицию
                 int currentChar = s.codePointAt(position);
 
-
-                position += currentChar;
+                if (currentChar == ' ' || currentChar == '\t') {
+                    trivialWhitespaces(s, sb, currentChar);
+                } else if (currentChar == '\n') {
+                    newLine(s, sb, currentChar);
+                } else if (currentChar == '#') {
+                    comment(s, sb, currentChar);
+                } else {
+                    sb.appendCodePoint(currentChar);            // TODO: для остальных поумнее
+                    position += Character.charCount(currentChar);
+                }
             }
+
+            scan(sb, 1, 1); //TODO: костыль
 
             return a;
         }
@@ -37,12 +100,13 @@ public class Main {
 
         ArrayList<Token> result = new ArrayList<>();
 
-        result = (ArrayList<Token>) MyLexer.lex("+ - =");
+        result = (ArrayList<Token>) MyLexer.lex("+ddf\t\t\t\t    -gfh      fdg    одав=#kjlfg sdgk;l");
 
         for (var token: result) {
 
-            System.out.println(token);
-        }
+                System.out.println(token);
+            }
+        System.out.println("lol");
     }
 
 }
---
+++ src/Scanner.java
@@ -0,0 +1,11 @@
+import syspro.tm.lexer.Token;
+
+public class Scanner {
+
+    public static Token scanToken(StringBuilder sb, int start, int end) {
+
+        System.out.println("token " + sb.toString() + ", length: " + sb.length());
+        return null;    //TODO: implement scanToken
+    }
+
+}
